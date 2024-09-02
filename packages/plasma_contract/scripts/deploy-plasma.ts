c0ada75e94d337eb4ad1e031cff88f01c9d334cc
Hussenet Thomas <Lejamon.c@protonmail.com>
2024-09-02 11:35:12 +0200

    repush contracts

---
+++ packages/plasma_contract/scripts/deploy-plasma.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const groth16Verifier = await ethers.deployContract("Groth16Verifier", []);
+  await groth16Verifier.waitForDeployment();
+  console.log("Groth16Verifier contract deployed to:", groth16Verifier.target);
+  const plasma = await ethers.deployContract("Plasma", [
+    groth16Verifier.target,
+  ]);
+  await plasma.waitForDeployment();
+  console.log("Plasma contract deployed to:", plasma.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
